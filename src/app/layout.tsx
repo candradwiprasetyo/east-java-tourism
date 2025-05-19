e4be71fd0024566198a04843f575b2991a7838cd
unknown <candradwiprasetyo@gmail.com>
2025-05-19 15:12:54 +0700

    fix: font issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,27 +5,27 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-  display: "optional",
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-  display: "optional",
+  display: "swap",
 });
 
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
   weight: ["400", "700"],
-  display: "optional",
+  display: "swap",
 });
 
 const satisfy = Satisfy({
   subsets: ["latin"],
   variable: "--font-satisfy",
   weight: ["400"],
-  display: "optional",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -44,12 +44,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons&display=optional"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons&display=swap"
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=swap"
           rel="stylesheet"
         />
       </head>
